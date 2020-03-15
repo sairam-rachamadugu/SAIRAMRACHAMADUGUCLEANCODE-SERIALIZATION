b61208133cebf9ed29ace1a2f3fe5c6b3d12ec2f
sairam-rachamadugu <ram.rachamadugu32@gmail.com>
2020-03-15 19:35:14 +0530

    commit2

---
+++ cleancode2/src/main/java/cleancode/cleancode2/App.java
@@ -0,0 +1,13 @@
+package cleancode.cleancode2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cleancode2/src/main/java/cleancode/cleancode2/compound_interest.java
@@ -0,0 +1,18 @@
+package cleancode.cleancode2;
+
+
+public class compound_interest {
+	double amount,noOfYears,interestRate;
+	public compound_interest(double amount, double noOfYears, double interestRate) {
+		// TODO Auto-generated constructor stub
+		this.amount=amount;
+		this.noOfYears=noOfYears;
+		this.interestRate=interestRate;
+	}
+
+	public double calculate1() {
+		// TODO Auto-generated method stub
+		return amount * Math.pow(1.0+interestRate/100.0,noOfYears) - amount;
+	}
+
+}
